1e71572b64b387825104284ab4e88063cfd78a38
Afin <testhubcode@gmail.com>
2022-01-29 23:20:15 +0400

    carousel commit

--- assets/script/carousel.js
+++ assets/script/carousel.js
@@ -1,20 +1,42 @@
-$('.owl-carousel').owlCarousel({
-    items:3,
-    loop:true,
-    margin:24,
-    // autoplay:true,
-    // autoplayTimeout:1500,
-    // autoplayHoverPause:true,
-    merge:false,
-    responsive:{
-        575:{
-            items: 1,
-            mergeFit:true,
-            margin: 5
-        },
-        1000:{
+// $('.owl-carousel').owlCarousel({
+//     items:3,
+//     loop:true,
+//     margin:24,
+//     autoplay:true,
+//     autoplayTimeout:1500,
+//     autoplayHoverPause:true,
+//     merge:false,
+//     responsive:{
+//         0:{
+//             items: 1,
+//             mergeFit:true,
+//             margin: 0
+//         },
+//         1000:{
             
-            mergeFit:false
+//             mergeFit:false
+//         }
+//     }
+// });
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 3,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: false,
+            dots:false,
+            loop: true
         }
     }
-});
+})
